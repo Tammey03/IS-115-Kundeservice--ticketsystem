82cf5f118c88f97cf62b20ba2c07bff0046d81a1
Tam Nguyen <tamnguyen@Tam-sin-Air.home>
2026-09-08 22:49:04 +0200

    Legge grunnlagt for prosjekten

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="no">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Kundeservice</title>
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    
+    <header>
+        <!-- Navigasjon kommer her -->
+    </header>
+
+    <main>
+        <h1>Kundeservice</h1>
+        <!-- Innhold kommer her -->
+    </main>
+
+    <footer>
+        <!-- Footer kommer her -->
+    </footer>
+
+    <script src="js/script.js"></script>
+</body>
+
+</html>
