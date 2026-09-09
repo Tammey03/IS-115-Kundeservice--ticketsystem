c4c946c219daabc645407916faa2b62b93568eea
Tam Nguyen <tamnguyen@Tam-sin-Air.home>
2026-09-09 12:44:32 +0200

    Legge til funksjonalitet iden

--- index.php
+++ index.php
@@ -18,9 +18,21 @@
 
     <main>
         <h1>Kundeservice</h1>
-      <p>Velkommen til vår kundeservice-side! Her kan du finne svar på vanlige spørsmål, kontakte oss for hjelp, og få informasjon om våre tjenester.</p>
-        <p>I dette prosjektet skal dere utvikle et webbasert kundeservice- og ticketsystem hvor kunder kan sende inn henvendelser, mens annsatte kan behandle, prioritere og svare på sakene gjennon et administrasjonspanel. 
+        <p>Velkommen til vår kundeservice-side! Her kan du finne svar på vanlige spørsmål, kontakte oss for hjelp, og få informasjon om våre tjenester.</p>
+       <p>I dette prosjektet skal dere utvikle et webbasert kundeservice- og ticketsystem hvor kunder kan sende inn henvendelser, mens annsatte kan behandle, prioritere og svare på sakene gjennon et administrasjonspanel. 
         Systemet skal gi oversikt over status på saker, hvem som behandler dem, og historikk over kommunikasjon. </p>
+        <p>Team: Studentbolig som trenger et kundeservice- og ticketsystem for å håndtere kundeservicehenvendelser. </p>
+    <p> Funksjonalitet: 
+        - Opprettelse av nye saker - Kunder kan sende in henvendelse via et webskjeme, som lagres i en database. 
+        - Behandling av saker - 
+        - Svar på saker
+        - Vise historikk og status over saker (f.eks nav, studentbolig)
+        - prioritering av saker (f.eks lav, middels, høy)
+
+
+    </p>
+
+
     </main>
 
     <footer>
