81eb831a1d181293861cef983a6f0fea02ec9a2d
Tam Nguyen <tamnguyen@Tam-sin-Air.home>
2026-09-09 13:06:29 +0200

    Legge til nødvendlige filer

---
+++ pages/admin/alleSaker.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="no">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Kundeservice</title>
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    
+    <header>
+        <!-- Navigasjon kommer her -->
+    </header>
+
+    <main>
+        <h1>Alle saker admin</h1>
+
+<a href="pages/bruker/mainUser.php" class="button">Gå til bruker-side</a>
+<a href="pages/admin/mainAdmin.php" class="button">Gå til admin-side</
+    </main>
+
+    <footer>
+        <!-- Footer kommer her -->
+    </footer>
+
+    <script src="js/script.js"></script>
+</body>
+
+</html>
---
+++ pages/bruker/opprettSak.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="no">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Kundeservice</title>
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    
+    <header>
+        <!-- Navigasjon kommer her -->
+    </header>
+
+    <main>
+        <h1>Opprette ny sak bruker</h1>
+</main>
+
+    <footer>
+        <!-- Footer kommer her -->
+    </footer>
+
+    <script src="js/script.js"></script>
+</body>
+
+</html>
